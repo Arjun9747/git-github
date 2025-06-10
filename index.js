f49b7766d931162a3ae3e7aa72b8fd3f6e9f78f0
Arjun9747 <arjunedakkad@gmail.com>
2025-06-10 07:28:22 +0000

    modified

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from Node.js App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
